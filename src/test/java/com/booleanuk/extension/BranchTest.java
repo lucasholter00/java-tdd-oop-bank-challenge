c6ac6373a4e8801b4d991245ac0c02a5ede8032d
Lucas Holter <lucas.holter@hotmail.com>
2025-08-12 15:29:20 +0200

    Add tests for branch

---
+++ src/test/java/com/booleanuk/extension/BranchTest.java
@@ -0,0 +1,49 @@
+package com.booleanuk.extension;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+import java.util.ArrayList;
+import java.util.UUID;
+
+public class BranchTest {
+
+    private Branch generateBranch(){
+        Branch branch = new Branch(new ArrayList<Account>(), new ArrayList<Request>());
+        branch.addAccount(new Account(new ArrayList<Transaction>(), UUID.randomUUID().toString(), 0));
+        return branch;
+    }
+
+    @Test
+    public void testCreateRequest(){
+        Branch branch = generateBranch();
+        Account acc = branch.getAccounts().get(0);
+        branch.createRequest(acc, 2000);
+        Assertions.assertEquals(1, branch.getRequests().size());
+    }
+
+    @Test
+    public void testAcceptRequest(){
+        Branch branch = generateBranch();
+        Account acc = branch.getAccounts().get(0);
+        branch.createRequest(acc, 2000);
+        Request req = branch.getRequests();
+
+        branch.acceptRequest(req);
+
+        Assertions.assertEquals(2000, acc.getOverdraft());
+    }
+
+    @Test
+    public void testDenyRequest(){
+        Branch branch = generateBranch();
+        Account acc = branch.getAccounts().get(0);
+        branch.createRequest(acc, 2000);
+        Request req = branch.getRequests();
+
+        branch.denyRequest(req);
+
+        Assertions.assertEquals(0, acc.getOverdraft());
+    }
+
+}
